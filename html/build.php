e0ffeae23c69f0ff072158e33c703c9ba7a14949
Michael Murrell <murrell_michael@yahoo.com>
2018-08-11 05:40:32 -0700

    work on build script

--- html/build.php
+++ html/build.php
@@ -3,12 +3,16 @@
 include 'config.php';
 
 // remove build folder
+use Symfony\Component\Filesystem\Filesystem;
+
+(new Filesystem)->remove(APP_DIR . '/../build');
 
 // mkdir build folder
+(new Filesystem)->mkdir(APP_DIR . '/../build');
 
 // Copy assets
 
-recurse_copy(APP_DIR . '/assets/', APP_DIR . '/../build/');
+(new Filesystem)->mkdir(APP_DIR . '/assets', APP_DIR . '/../build');
 
 foreach ($pages as $page) {
 
@@ -24,6 +28,9 @@ foreach ($pages as $page) {
 
     $output = ob_get_clean();
 
-    file_put_contents("$page.html", $output);
+    $put_to = APP_DIR . '/../build/' . "$page.html";
+    error_log($put_to);
+
+    file_put_contents($put_to, $output);
 
 }
